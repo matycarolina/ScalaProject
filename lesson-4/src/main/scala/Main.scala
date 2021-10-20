1244349c587465198217ee50dd49eadc2abc65cc
Mariale <mcontreras@talenteca.io>
2021-10-20 18:20:56 -0500

    Update file with exercise of lesson 6

--- lesson-4/src/main/scala/Main.scala
+++ lesson-4/src/main/scala/Main.scala
@@ -19,11 +19,25 @@ object Main extends App {
     else { n * 0.85 }
   }
 
-  println("El precio es: " + discount(30))
+  /* println("El precio es: " + discount(30))
   println("El precio es: " + discount(80))
   println("El precio es: " + discount(130))
 
   println("El precio es: " + descuento(30))
   println("El precio es: " + descuento(80))
-  println("El precio es: " + descuento(130))
+  println("El precio es: " + descuento(130)) */
+
+  //Ejercicio leccion  6
+  //Solucion Mariale
+  def powFunc(exponent: Int, base: Int = 2): Int = {
+    var res = 1
+    for (i <- 1 to exponent) {
+      println(res)
+      res *= base
+    }
+    println("El resultado con pow es: " + scala.math.pow(base,exponent))
+    res
+  }
+  println("El resultado con mi funcion es: " + powFunc(2,3))
+
 }
